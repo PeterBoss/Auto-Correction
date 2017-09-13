ce3feae39992f264e4b9b0db0b415feac084d072
PeterBoss <peter2137@gmail.com>
2017-09-13 11:12:44 +0200

    entity stuff

---
+++ src/main/java/entity/User.java
@@ -0,0 +1,47 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Peter
+ */
+@Entity
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private short skillLevel;  //think of better thing to call it, maybe
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public short getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(short skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+}
